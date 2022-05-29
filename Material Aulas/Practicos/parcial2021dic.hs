e62955e0db800bc55ba6aaed9a2784799fee42c5
Lucas Viera <lucasviera22@gmail.com>
2022-05-29 18:21:36 -0300

    Parcial empezar en clase

---
+++ Material Aulas/Practicos/parcial2021dic.hs
@@ -0,0 +1,8 @@
+alternancia:: a -> a -> [a] -> Bool
+alternancia = \x y l -> case l of {[] -> ;
+                                   z:zs -> case z == x of  {False -> False; True -> alternancia y x zs}}
+
+data N where {O::N;S::N->N} deriving Show 
+altGen :: N -> a -> a -> [a]
+altGen = \n x y -> case n of {O -> [];
+                             S z -> x : altGen z y x}
