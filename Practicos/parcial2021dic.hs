02d1fbe0cf38b580863597f0209f977447196c39
Lucas Viera <lucasviera22@gmail.com>
2022-06-12 18:47:28 -0300

    Parcial visto en clase

--- Practicos/parcial2021dic.hs
+++ Practicos/parcial2021dic.hs
@@ -1,6 +1,7 @@
 alternancia:: a -> a -> [a] -> Bool
 alternancia = \x y l -> case l of {[] -> ;
-                                   z:zs -> case z == x of  {False -> False; True -> alternancia y x zs}}
+                                   z:zs -> case z == x of  {False -> False; 
+                                                            True -> alternancia y x zs}}
 
 data N where {O::N;S::N->N} deriving Show 
 altGen :: N -> a -> a -> [a]
