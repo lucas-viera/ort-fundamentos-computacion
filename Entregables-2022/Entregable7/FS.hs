c2158d194fb37ca05950acd4e4f94c221ee5df19
Lucas Viera <lucasviera22@gmail.com>
2022-06-26 11:29:20 -0300

    funciona pertenece ej 5

--- Entregables-2022/Entregable7/FS.hs
+++ Entregables-2022/Entregable7/FS.hs
@@ -48,7 +48,7 @@ csys :: FS
 csys = C "sys" [A ("sys", Txt), C "sys" []]
 
 csysvacio :: FS
-csysvacio = C "sys" []
+csysvacio = C "sys" [A ("p2", Jar)]
 
 craiz :: FS
 craiz = C "raiz" [cmusica, A ("notas", Txt), cort, csys]
@@ -57,35 +57,49 @@ craiz = C "raiz" [cmusica, A ("notas", Txt), cort, csys]
 -- 2
 nombre :: FS -> Nombre
 nombre = \fs -> case fs of { A (n, e) -> n ++ "." ++ show e;
-                             C nom fss ->  nom
-						   }
+                             C nom fss ->  nom }
 ----
 -- 3
-
 contenido :: FS -> [Nombre]
 contenido = \fs -> case fs of { A (n, e) -> error "no es carpeta";
-	                            C n fss -> case fss of {				-- C n [fss] seria el FS tipo carpeta a 'recorrer'
-									                    [] -> [];		--caso para una carpeta vacia
-														x:xs -> [nombre x] ++ contenido (C "" xs)		--caso con un elemento x (de tipo FS) y concatenacion con un nuevo FS simulado con el resto de elementos
-														}
-                              }
-
+	                            C n fss -> case fss of {[] -> [];		
+														x:xs -> [nombre x] ++ contenido (C "" xs) }}
 ----
 -- 4
 cantA :: FS -> Int 
-cantA = undefined
+cantA = \fs -> case fs of { A (n, e) -> 1;
+	                        C n fss -> case fss of { [] -> 0;
+								                     x:xs -> case x of { A (n2, e2) -> 1 + cantA (C "" xs) ;
+													                     C n2 fss2 -> cantA (C "" xs) + cantA (C "" fss2) }}}
 ----
 -- 5
 pertenece :: Nombre -> FS -> Bool 
-pertenece = undefined
+pertenece = \nom fs -> case fs of { A (n,e) -> n==nom ;
+									C n fss -> case fss of {[] -> False;
+									                        x:xs ->  case x of { A (n2,e2) -> n2==nom || pertenece nom (C nom xs);
+															                     C n2 fss2 -> n2==nom || pertenece nom (C nom fss2) || pertenece nom (C nom xs) }}}
 ----
 -- 6
 valido :: FS-> Bool
-valido = undefined
+valido = \fs -> case fs of { A (n,e) -> True;								--caso con un solo archivo
+	                         C n fss -> case fss of {[] -> True; 			--caso carpeta vacia"
+							                          x:xs -> case x of { A (n2,e2) -> not(n2 == nombre x)  && valido (C "" xs);
+													                      C n2 fss2 -> valido (C "" xs)}}}  --not(n2 == nombre x) && 
+																		  
+																		  
+																		  
+																		  
+																		  
 ----
 -- 7
 archivosExt :: Ext -> FS -> [Nombre]
 archivosExt = undefined
+-- archivosExt = \ext fs -> case fs of { A (n, e) -> case e==ext of {True -> [n];
+-- 																  False -> [] };
+--                                       C n fss -> case fss of { [] -> [];
+-- 									                           x:xs -> case x of {A (n2, e2) -> case e2 == ext of {True -> n2 : archivosExt ext (C "" xs);
+-- 															                                                       False -> archivosExt ext (C "" xs) }
+-- 																				  C n3 e3 -> archivosExt}}}
 ----
 -- 8
 cambiarNom :: Nombre -> Nombre -> FS -> FS 
